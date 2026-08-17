da0d27eb065809d5b6acf6b40769a8b5043ce138
SBF Rusho <102538412+sbfrusho@users.noreply.github.com>
2026-08-17 13:48:45 +0600

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+
+        for(char ch : s1) {
+            mp1[ch]++;
+        }
+        
+        int window_length = s1.length();
+
+        for(int i = 0 ; i < window_length ; i++) {
+            mp2[s2[i]]++;
+        }
+        if(mp1 == mp2) return true;
+
+        for(int r = window_length ; r < s2.size() ; r++) {
+            mp2[s2[r]]++;
+            int l = r - window_length;
+            mp2[s2[l]]--;
+            if(mp2[s2[l]] == 0) {
+                mp2.erase(s2[l]);
+                l++;
+            }
+            if (mp1 == mp2) return true;
+        }
+        return false;
+    }
+};
